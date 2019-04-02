755408ba7d45a01f895143d57276180426f853a8
agmoss <agordonmoss@gmail.com>
2019-04-02 16:32:48 -0600

    Running as a jar

--- src/main/java/com/agmoss/ClassicModelsRestApi/controller/OrderDetailsController.java
+++ src/main/java/com/agmoss/ClassicModelsRestApi/controller/OrderDetailsController.java
@@ -1,17 +1,14 @@
 package com.agmoss.ClassicModelsRestApi.controller;
 
-import com.agmoss.ClassicModelsRestApi.exception.ResourceNotFoundException;
 import com.agmoss.ClassicModelsRestApi.model.OrderdetailsEntity;
-import com.agmoss.ClassicModelsRestApi.model.OrdersEntity;
 import com.agmoss.ClassicModelsRestApi.repository.OrderDetailsRepository;
 import com.agmoss.ClassicModelsRestApi.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,10 +18,7 @@ public class OrderDetailsController {
     @Autowired
     private OrderDetailsRepository orderDetailsRepository;
 
-    @Autowired
-    private OrderRepository orderRepository;
 
-    //@GetMapping("/orders/{orderId}/orderdetails")
     @RequestMapping(method = RequestMethod.GET, value = "/orders/{ordernumber}/orderdetails")
     public List<OrderdetailsEntity> getAllOrderDetailsByOrderId(@PathVariable (value = "ordernumber") Integer orderId){
 
--- src/main/java/com/agmoss/ClassicModelsRestApi/repository/OrderDetailsRepository.java
+++ src/main/java/com/agmoss/ClassicModelsRestApi/repository/OrderDetailsRepository.java
@@ -1,14 +1,13 @@
 package com.agmoss.ClassicModelsRestApi.repository;
 
 import com.agmoss.ClassicModelsRestApi.model.OrderdetailsEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
+@Component
 @Repository
 public interface OrderDetailsRepository extends JpaRepository<OrderdetailsEntity,Integer> {
 
